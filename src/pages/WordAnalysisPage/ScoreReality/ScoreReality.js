0ffb1cbc0ea771ae2f5162b5b09ac6bc618b72ea
Alan Chuang <weiyu.chuang@flowring.com>
2023-10-13 16:58:00 +0800

    update

--- src/pages/WordAnalysisPage/ScoreReality/ScoreReality.js
+++ src/pages/WordAnalysisPage/ScoreReality/ScoreReality.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { DataTable } from 'mantine-datatable';
+import { useMantineTheme } from "@mantine/core";
+import classes from "./ScoreReality.module.scss";
 
 // datatable document
 // https://icflorescu.github.io/mantine-datatable/examples/basic-usage
@@ -7,13 +9,11 @@ import { DataTable } from 'mantine-datatable';
 const PAGE_SIZE = 10;
 
 const ScoreReality = () => {
-
-    const employees = [
-        { firstName: "Alan", lastName: "Chuang1111111111 111111111111111111111", email: "a135@gmail.com" }
-    ];
+    const theme = useMantineTheme();
+    const employees = [];
 
     for (let index = 0; index < 20; index++) {
-        employees.push({ firstName: "Alan", lastName: "Chuang" });
+        employees.push({ rank: index, lastName: "Chuang" });
     }
 
     const [page, setPage] = useState(1);
@@ -27,32 +27,37 @@ const ScoreReality = () => {
 
 
     return (
-        <DataTable
-            style={{ backgroundColor: "transparent" }}
-            highlightOnHover
-            borderRadius="md"
-            height={300}
-            withBorder
-            records={records}
-            columns={[
-                { accessor: 'firstName', width: 100 },
-                { accessor: 'lastName', width: 100 },
-            ]}
-            totalRecords={employees.length}
-            recordsPerPage={PAGE_SIZE}
-            page={page}
-            onPageChange={(p) => setPage(p)}
-        // uncomment the next line to use a custom loading text
-        // loadingText="Loading..."
-        // uncomment the next line to display a custom text when no records were found
-        // noRecordsText="No records found"
-        // uncomment the next line to use a custom pagination text
-        // paginationText={({ from, to, totalRecords }) => `Records ${from} - ${to} of ${totalRecords}`}
-        // uncomment the next line to use a custom pagination color (see https://mantine.dev/theming/colors/)
-        // paginationColor="grape"
-        // uncomment the next line to use a custom pagination size
-        // paginationSize="md"
-        />
+        <div className={classes["table-wrapper"]}>
+            <DataTable
+                styles={{
+                    header: { backgroundColor: theme.colorScheme === "dark" ? theme.colors.brand[0] : "#fff" },
+                    pagination: { backgroundColor: "transparent", }
+                }}
+                highlightOnHover
+                borderRadius="md"
+                height={300}
+                withBorder
+                records={records}
+                columns={[
+                    { accessor: 'rank', width: 30 },
+                    { accessor: 'lastName', width: 150, ellipsis: true, },
+                ]}
+                totalRecords={employees.length}
+                recordsPerPage={PAGE_SIZE}
+                page={page}
+                onPageChange={(p) => setPage(p)}
+            // uncomment the next line to use a custom loading text
+            // loadingText="Loading..."
+            // uncomment the next line to display a custom text when no records were found
+            // noRecordsText="No records found"
+            // uncomment the next line to use a custom pagination text
+            // paginationText={({ from, to, totalRecords }) => `Records ${from} - ${to} of ${totalRecords}`}
+            // uncomment the next line to use a custom pagination color (see https://mantine.dev/theming/colors/)
+            // paginationColor="grape"
+            // uncomment the next line to use a custom pagination size
+            // paginationSize="md"
+            />
+        </div>
     );
 };
 

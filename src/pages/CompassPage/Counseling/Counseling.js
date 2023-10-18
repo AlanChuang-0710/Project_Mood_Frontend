a13829fd93fe1db5bac95c2d0755730358a00326
Alan Chuang <weiyu.chuang@flowring.com>
2023-10-18 15:22:55 +0800

    update

--- src/pages/CompassPage/Counseling/Counseling.js
+++ src/pages/CompassPage/Counseling/Counseling.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Grid, } from "@mantine/core";
 import { useGetComponentStyle } from "../../../styles/dayNightStyle";
 import Map from './Map/Map';

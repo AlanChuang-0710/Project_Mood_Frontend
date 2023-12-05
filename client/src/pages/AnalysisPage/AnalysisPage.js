7adf36b200031069ebad4c1463907b08ebd5dcb7
Alan Chuang <weiyu.chuang@flowring.com>
2023-12-05 11:40:15 +0800

    feat(analysis): 新增sleepHistogram; fix(sleepScoreBar): 修正星期排序

--- client/src/pages/AnalysisPage/AnalysisPage.js
+++ client/src/pages/AnalysisPage/AnalysisPage.js
@@ -7,6 +7,7 @@ import ScoreDayBarChart from './ScoreDayBarChart/ScoreDayBarChart';
 import SleepFlowChart from './SleepFlowChart/SleepFlowChart';
 import ScoreAssociatedChart from "./ScoreAssociatedChart/ScoreAssociatedChart";
 import SleepScoreScatterChart from './SleepScoreScatterChart/SleepScoreScatterChart';
+import SleepHistogram from "./SleepHistogram/SleepHistogram";
 import { useGetScorePieChartDataQuery, useGetScoreLineChartDataQuery, useGetScoreDayBarDataQuery, useGetSleepLineChartDataQuery, useGetTagsScoreDataQuery, useGetKOLScoreDataQuery } from '../../store/api/analysisApi';
 import { selectCurrentUserId } from "../../store/reducer/authSlice";
 import { useGetComponentStyle } from "../../styles/dayNightStyle";
@@ -45,7 +46,7 @@ const AnalysisPage = () => {
       let depressionRatio = (scorePieChartData.data.data[0].count / scorePieChartData.data.total).toFixed(2) * 100;
     };
 
-    // 波動率 (介於0 ~ 2之間)
+    // 波動率 (介於0 ~ 2之間)，大於1.5認定為波動較大。
     if (scoreFlowChartData) {
       let scoreArray = [];
 
@@ -58,7 +59,7 @@ const AnalysisPage = () => {
         const n = data.length;
 
         if (n === 0) return 0;
-        
+
         // 計算平均值
         const mean = data.reduce((sum, value) => sum + value, 0) / n;
 
@@ -71,16 +72,30 @@ const AnalysisPage = () => {
         return standardDeviation;
       };
       let scoreVolatility = calculateStandardDeviation(scoreArray);
+      console.log(`過去一段時間您的心情波動率約為${scoreVolatility}`);
     }
 
-    // 星期幾低潮佔比較高，建議可以特別注意
+    // 星期幾低潮佔比較高，建議可以特別注意 (特定星期幾depressed比例高於特定星期幾的1/2，則認定特定星期幾具有較高比例不開心)
+    if (scoreDayBarChartData) {
+      const dayArrMapping = ["Sunday", "Saturday", "Friday", "Thursday", "Wednesday", "Tuesday", "Monday"];
+      let overDepressedArr = [];
+      scoreDayBarChartData.data.barData[0].forEach((item, index) => {
+        if (item / (scoreDayBarChartData.data.total / 7) >= 0.5) {
+          overDepressedArr.push(dayArrMapping[index]);
+        };
+      });
+      console.log(overDepressedArr.join(", ") + "有較高的低潮比例，需特別注意!");
+    }
 
     // 睡眠時段主要分布範圍 (幾個小時到幾個小時之間)
+    if (sleepFlowChartData) {
+
+    }
 
     // 結合睡眠與心情時間圖，進行綜合分析，可以發現___
 
 
-  }, [scorePieChartData, scoreFlowChartData]);
+  }, [scorePieChartData, scoreFlowChartData, scoreDayBarChartData]);
 
   return (
     <div>
@@ -124,10 +139,15 @@ const AnalysisPage = () => {
         </Grid.Col>
         <Grid.Col xs={12} md={4}>
           <div style={useGetComponentStyle()}>
+            <SleepHistogram height={350} sleepFlowChartData={sleepFlowChartData} />
+          </div>
+        </Grid.Col>
+        <Grid.Col xs={12} md={4}>
+          <div style={useGetComponentStyle()}>
             <SleepScoreScatterChart height={350} sleepFlowChartData={sleepFlowChartData} scoreFlowChartData={scoreFlowChartData} />
           </div>
         </Grid.Col>
-        <Grid.Col xs={12} md={8}>
+        <Grid.Col xs={12} md={4}>
           <div style={useGetComponentStyle()}>
             <div className={classes["score-report1"]}>
               過去一{value === "month" ? "個月" : "年"}當中，XXX總共記錄了___天心情筆記。從筆記中，我們發現開心佔比約___%；低潮佔比約___%。

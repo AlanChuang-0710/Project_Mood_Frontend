f60311bfefb823866eace681a878dd801e43e2dc
Alan <a0976468558@gmail.com>
2023-12-28 22:24:58 +0800

    refactor(utils request): 新建request統一管理獲得refresh token

--- client/src/store/api/commonApi.js
+++ client/src/store/api/commonApi.js
@@ -1,36 +1,6 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
-import { setCredentials, logout } from "../reducer/authSlice";
+import { createApi } from "@reduxjs/toolkit/dist/query/react";
+import { baseQueryWithReauth } from "../../utils/request";
 
-//指定查詢的基礎信息，發信請求的工具
-const baseQuery = fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:3000/common",
-    credentials: "include", // 即便跨域也會攜帶上cookie
-    mode: 'cors',
-    timeout: 10000,
-    // 統一修改請求頭 參數包括(headers && redux store倉庫)
-    prepareHeaders: (headers, { getState }) => {
-        const accessToken = getState().auth.accessToken;
-        if (accessToken) {
-            headers.set("accessToken", accessToken);
-        }
-        return headers;
-    }
-});
-
-const baseQueryWithReauth = async (args, api, extraOptions) => {
-    let result = await baseQuery(args, api, extraOptions);
-    if (result?.data?.data?.message === "jwt expired") {
-        const refreshResult = await fetchBaseQuery({ baseUrl: "http://127.0.0.1:3000", credentials: "include", mode: 'cors' })('/users/refresh', api, extraOptions);
-        if (refreshResult?.data?.data?.accessToken) {
-            const authData = api.getState().auth;
-            api.dispatch(setCredentials({ ...authData, accessToken: refreshResult.data.data.accessToken })); // store new token
-            result = await baseQuery(args, api, extraOptions); // retry original query with new access token
-        } else {
-            api.dispatch(logout());
-        }
-    }
-    return result;
-};
 
 const commonApi = createApi({
 
@@ -38,7 +8,7 @@ const commonApi = createApi({
     reducerPath: "common",
 
     //指定查詢的基礎信息，發信請求的工具
-    baseQuery: baseQueryWithReauth,
+    baseQuery: baseQueryWithReauth("http://127.0.0.1:3000", "/common", "/users/refresh"),
 
     tagTypes: ["getCommonData"], //用來指定Api中的標籤類型
 
---
+++ client/src/utils/request.js
@@ -0,0 +1,34 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { setCredentials, logout } from "../store/reducer/authSlice";
+
+//指定查詢的基礎信息，發信請求的工具
+const baseQuery = (url) => fetchBaseQuery({
+    baseUrl: url,
+    credentials: "include", // 即便跨域也會攜帶上cookie
+    mode: 'cors',
+    timeout: 10000,
+    // 統一修改請求頭 參數包括(headers && redux store倉庫)
+    prepareHeaders: (headers, { getState }) => {
+        const accessToken = getState().auth.accessToken;
+        if (accessToken) {
+            headers.set("accessToken", accessToken);
+        }
+        return headers;
+    }
+});
+
+export const baseQueryWithReauth = (baseURL, targetURLSuffix, refreshURLSuffix) => async (args, api, extraOptions) => {
+    let result = await baseQuery(`${baseURL}${targetURLSuffix}`)(args, api, extraOptions);
+    if (result?.data?.data?.message === "jwt expired") {
+        const refreshResult = await fetchBaseQuery({ baseURL, credentials: "include", mode: 'cors' })(`${refreshURLSuffix}`, api, extraOptions);
+        if (refreshResult?.data?.data?.accessToken) {
+            const authData = api.getState().auth;
+            api.dispatch(setCredentials({ ...authData, accessToken: refreshResult.data.data.accessToken })); // store new token
+            result = await baseQuery(args, api, extraOptions); // retry original query with new access token
+        } else {
+            api.dispatch(logout());
+        }
+    }
+    return result;
+};
+

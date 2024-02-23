59ca79a3809201d9b9181ff5f87d2523b5ca9f69
Alan Chuang <weiyu.chuang@flowring.com>
2024-02-23 10:01:28 +0800

    chore(BPserver): 設置全局變量

--- BPserver/config/config.js
+++ BPserver/config/config.js
@@ -1,17 +1,20 @@
 // 配置文件
+const path = require("path");
+require('dotenv').config({ path: path.resolve(__dirname, '../env/.env.' + process.env.NODE_ENV) });
+
 module.exports = {
-    DBHOST: "127.0.0.1",
-    DBPORT: 5432,
-    DBNAME: "mood_bp_db",
-    PASSWORD: "postgres",
-    USER: "postgres",
-    SERVERHOST: "http://127.0.0.1",
-    SERVERPORT: 3002,
-    FRONTENDHOST: "http://localhost",
-    FRONTENDPORT: 8550,
-    APIDBHOST: "mongodb://127.0.0.1",
-    APIDBPORT: 27017,
-    APIDBNAME: "Mood",
-    ACCESS_TOKEN_SECRET: "6c0b7ed3b2343e2867c88e29a06116cd1d42fe961ee632598f28e21edf02c9dcd2e4303ab22480a88aa7eb5aa236ff066be22b28a0a5da884643df53a46b837f",
-    REFRESH_TOKEN_SECRET: "2d41eacaa6e1133a194c0646d94a6eb9ec72adf727f6f4b71aca6bc22b36e2bcae7f80b98b595f79dbf3b96d5432958f22e4485c60bd571ef9bf395634357fef"
+    DBHOST: process.env.DBHOST,
+    DBPORT: process.env.DBPORT,
+    DBNAME: process.env.DBNAME,
+    PASSWORD: process.env.PASSWORD,
+    USER: process.env.USER,
+    SERVERHOST: process.env.SERVERHOST,
+    SERVERPORT: process.env.SERVERPORT,
+    FRONTENDHOST: process.env.FRONTENDHOST,
+    FRONTENDPORT: process.env.FRONTENDPORT,
+    APIDBHOST: process.env.APIDBHOST,
+    APIDBPORT: process.env.APIDBPORT,
+    APIDBNAME: process.env.APIDBNAME,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET
 };
